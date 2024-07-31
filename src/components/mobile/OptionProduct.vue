73607b4ae3739e1a1fca1f9147a36e6d25d3cf49
aphinet12T <aphinet.dam@onetwotrading.co.th>
2024-07-31 13:39:37 +0700

    Update OptionProduct.vue

--- src/components/mobile/OptionProduct.vue
+++ src/components/mobile/OptionProduct.vue
@@ -7,7 +7,7 @@
                         <select v-model="selectedGroup" @change="emitData"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[170px] p-2.5">
                             <option value="" disabled selected class="text-center">ประเภท</option>
-                            <option v-for="group in dataProduct.group" :key="group" :value="group">
+                            <option v-for="group in dataProduct.group" :key="group" :value="group" class="text-center">
                                 {{ group }}
                             </option>
                         </select>
@@ -18,7 +18,7 @@
                         <select v-model="selectedBrand" @change="emitData"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[170px] p-2.5">
                             <option value="" disabled selected class="text-center">แบรนด์</option>
-                            <option v-for="brand in dataProduct.brand" :key="brand" :value="brand">
+                            <option v-for="brand in dataProduct.brand" :key="brand" :value="brand" class="text-center">
                                 {{ brand }}
                             </option>
                         </select>
@@ -31,7 +31,7 @@
                         <select v-model="selectedSize" @change="emitData"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[170px] p-2.5">
                             <option value="" disabled selected class="text-center">ขนาด</option>
-                            <option v-for="size in dataProduct.size" :key="size" :value="size">
+                            <option v-for="size in dataProduct.size" :key="size" :value="size" class="text-center">
                                 {{ size }}
                             </option>
                         </select>
@@ -42,7 +42,7 @@
                         <select v-model="selectedFlavour" @change="emitData"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[170px] p-2.5">
                             <option value="" disabled selected class="text-center">รสชาติ</option>
-                            <option v-for="flavour in dataProduct.flavour" :key="flavour" :value="flavour">
+                            <option v-for="flavour in dataProduct.flavour" :key="flavour" :value="flavour" class="text-center">
                                 {{ flavour }}
                             </option>
                         </select>
